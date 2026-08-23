9acccc14e518518433e77d7b64f06d3921f4f0dd
Luna Luna <felipelunaotaku115@gmail.com>
2026-08-23 17:13:34 -0600

    tool for converting images

---
+++ png_to_webp.py
@@ -0,0 +1,18 @@
+import os
+from PIL import Image
+
+def batch_convert_png_to_webp(folder_path, quality=85):
+    for filename in os.listdir(folder_path):
+        if filename.lower().endswith(".png"):
+            png_path = os.path.join(folder_path, filename)
+            webp_filename = os.path.splitext(filename)[0] + ".webp"
+            webp_path = os.path.join(folder_path, webp_filename)
+            
+            with Image.open(png_path) as img:
+                img.save(webp_path, "webp", quality=quality)
+            print(f"Converted: {filename} -> {webp_filename}")
+
+batch_convert_png_to_webp("./public/images/cheems")
+batch_convert_png_to_webp("./public/images/hit")
+batch_convert_png_to_webp("./public/images/music")
+
